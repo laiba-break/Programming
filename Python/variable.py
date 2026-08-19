6337352835f7b3beea75b36089b527b506cc73c2
Laiba Memon <memonlaiba03@gmail.com>
2026-08-19 13:52:59 +0200

    Variables

--- Python/variable.py
+++ Python/variable.py
@@ -1,3 +1,25 @@
-name = "bro"
-print (name)
+#first_name = "bro"
+#last_name = "code"
+#full_name = first_name + " " + last_name
+#print ("Hello, " + full_name)
+#This is a comment 
+#print (name)
+#now we do integ
 
+age= "21"
+#age = age + 1
+print (age)
+print(type(age))
+print("Your age is "+str(age))
+
+#float 
+height= 250.5
+print(height)
+print(type(height))
+print("Your height is "+str(height)+"cm")
+
+#boolean
+human = True
+print(human)
+print(type(human))
+print("Are you a human? "+str(human))
