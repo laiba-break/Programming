3eec6a8f07e8f371d1115b20bc9238e3d9847727
Laiba Memon <memonlaiba03@gmail.com>
2026-08-19 13:57:11 +0200

    Mutliple Assign

---
+++ Python/multipleassign.py
@@ -0,0 +1,12 @@
+#multiple assignment in python allows us to assignmultiple variables in one line of code
+#name = "Bro"
+#age = 21
+#attractiveness = True
+#below is for one line of code
+name, age, attractiveness = "Bro", 21, True
+print(name)
+print(age)
+print(attractiveness)
+
+Spongebod = Patrick = Squidward = "Bikini Bottom"
+print(Spongebod)
