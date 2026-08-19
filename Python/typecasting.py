80119578a94d49bc2223165cdd7d3ce4ca7b85a6
Laiba Memon <memonlaiba03@gmail.com>
2026-08-19 14:07:36 +0200

    typecasting

---
+++ Python/typecasting.py
@@ -0,0 +1,12 @@
+'typecasting= refers to converting the data type of variable from one type to another type'
+
+x = 1 #int
+y = 2.8 #float
+z = "3" #string
+z = int(z) #convert string to int
+x = float(x) #convert int to float
+y = str(y) #convert float to string
+print(y)
+print(x)
+print(z*3)
+      
