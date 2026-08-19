7e197e35682fedcbbea7c689157b474e6da76b31
Laiba Memon <memonlaiba03@gmail.com>
2026-08-19 18:25:23 +0200

    While Loop

---
+++ Python/whileloops.py
@@ -0,0 +1,7 @@
+#while loop = is a block of code that will execute as long as a condition is true.
+#while 1==1:
+ #   print("This will print forever")
+name = ""
+while len(name)== 0:
+    name = input("Enter your name: ")
+    print("Hello " + name + "!")
