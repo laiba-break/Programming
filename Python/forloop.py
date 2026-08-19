b02e8f1a033567467ef7449cf863db5006fb9aa2
Laiba Memon <memonlaiba03@gmail.com>
2026-08-19 18:30:59 +0200

    For Loops

---
+++ Python/forloop.py
@@ -0,0 +1,17 @@
+import time
+#for loop = a statement that will execute a block of code a limited amount of times.
+#while loop = unlimited amount of times.
+
+#for i in range(10):
+  # print(i+1)
+#for i in range(50,100,2):
+   
+#int(i+1)
+
+for i in "Laiba":
+    print(i)
+
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+print("Happy New Year!")    
