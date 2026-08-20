2d26103dc3937467ed75ccaf86607c1ff94b8154
Laiba Memon <memonlaiba03@gmail.com>
2026-08-20 13:01:28 +0200

    Nested Function

---
+++ Python/nestedfunction.py
@@ -0,0 +1,13 @@
+#nested function calls = function calls  inside other functionn calls.
+#  The inner function is executed first,
+#  and its result is used as an argument for the outer function.
+
+#num = input("Enter a whole positive number: ")
+#num = float(num)
+#num = abs(num)
+#num = round(num)
+#print(num)
+
+print((round(abs(float(input("Enter a whole positive number:"))))))
+
+            
