f4a1e6a3daa600154326832847a42c3685a471b4
Laiba Memon <memonlaiba03@gmail.com>
2026-08-20 12:06:53 +0200

    2D Lists

---
+++ Python/2Dlists.py
@@ -0,0 +1,8 @@
+#2D a list of lists
+
+drinks = ["coffee", "soda", "tea"]
+dinner = ["pizza", "hamburger", "hotdog"]
+dessert = ["ice cream", "pudding"]  
+# we have three lists, now we can make a list of lists
+food = [drinks, dinner, dessert]
+print(food[2][0]) #prints hamburger
