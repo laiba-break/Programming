5765e458c8c7514a4d34d8624a371662593ab1ed
Laiba Memon <memonlaiba03@gmail.com>
2026-08-20 15:54:00 +0200

    Copy Files

---
+++ Python/copyfile.py
@@ -0,0 +1,8 @@
+# copyfile() =  copies contents of a file
+# copy() = copyfile() + permission mode and destruction can be directory
+# copy2()
+
+import shutil
+
+#shutil.copyfile("file.txt","copy.txt")
+shutil.copy2("file.txt",r"C:\Users\LAIBA MEMON\Downloads\SKills_Laiba\Python")
