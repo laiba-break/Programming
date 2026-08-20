bb5c9df04329908c0ef505485a2559fb014fbcb6
Laiba Memon <memonlaiba03@gmail.com>
2026-08-20 12:02:24 +0200

    List

---
+++ Python/list.py
@@ -0,0 +1,17 @@
+#list = used to store multiple items in a single variable
+food = ["pizza", "hamburger", "hotdog", "spaghetti", "pudding"]
+#print(food[0])
+#for x in food:
+  #  print(x)
+
+food.append("ice cream")
+food.remove("hotdog")
+print(food)
+food.pop()
+print(food)
+food.insert(0, "cake")
+print(food)
+food.sort
+print(food)
+food.clear()
+print(food)
