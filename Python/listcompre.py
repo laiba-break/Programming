3df44617093811b3b077d26200e85b981bdefdb2
Laiba Memon <memonlaiba03@gmail.com>
2026-08-22 16:09:19 +0200

    List

---
+++ Python/listcompre.py
@@ -0,0 +1,22 @@
+#list comprehensions = a way to create a list with less syntax
+# can micmic certain lamda functions, easier to read
+# list= [expression for items in iterable]
+
+squares = []     #create a empty list
+for i in range(1,11): #create for loop
+    squares.append(i * i)  #define what each loop iteration should do
+print(squares)
+
+squares =  [i * i for i in range(1,11)]
+print(squares)
+
+#this took lesser code to tell how to fill list
+students = [100,90,80,70,60,50,40,30,9]
+passed_students = list(filter(lambda x: x>= 60, students))
+
+print(passed_students)
+
+passed_students= [i for i in students if i>= 60]
+print(passed_students)
+
+
