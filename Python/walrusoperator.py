048153e54704039d2b66109d18a09262506c9615
Laiba Memon <memonlaiba03@gmail.com>
2026-08-22 14:25:26 +0200

    Walrus Operator

---
+++ Python/walrusoperator.py
@@ -0,0 +1,26 @@
+#walrus operator
+#new in python
+#assignment expression aka walrus oeprator#
+#assigns values to variables as a part of the larger expression
+
+# happy = True
+# print(happy)
+
+print(happy:= True)
+
+#food= list()
+#while True:
+ #  food = input("What food do u like:")
+  # if food == "quit":
+   #    break
+   #food.append(food)
+
+#now see the other
+foods= list()
+while food:= input("What food do you like: ") != "quit":
+    foods.append(food)
+    
+
+
+
+
