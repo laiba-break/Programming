9a3860b8625dd9ec5904e71f1329e0b9ebb7e7b1
Laiba Memon <memonlaiba03@gmail.com>
2026-08-23 14:05:05 +0200

    Buttons

---
+++ Python/buttons.py
@@ -0,0 +1,29 @@
+#buttons = u click them and they do stuff 
+
+from tkinter import *
+
+def click():
+    global count
+    count = count +1
+    print(count)
+
+
+windows = Tk()
+
+photo= PhotoImage(file=r"C:\Users\LAIBA MEMON\Downloads\fairytail.png")
+
+button =Button(windows,text="Click me",
+               command=click,
+               font=("Comic Sans",30)
+               ,fg= "Green",
+               bg = "black",
+               activeforeground="Green",
+               activebackground="black",
+               state= ACTIVE,image= photo,
+               compound="top")
+
+
+
+button.pack()
+
+windows.mainloop()
