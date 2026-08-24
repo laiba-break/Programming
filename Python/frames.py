79026a16c2e68f7596221f40525580f6a71016ac
Laiba Memon <memonlaiba03@gmail.com>
2026-08-24 13:02:19 +0200

    Frames

---
+++ Python/frames.py
@@ -0,0 +1,16 @@
+#frames = a rectangular container to group and hold widgets 
+
+from tkinter import *
+
+window = Tk()
+
+frame = Frame(window,bg= "pink", relief=SUNKEN)
+frame.pack(side= BOTTOM)
+
+button = Button(frame,text ="W",font = ("Consoles",25),width=3).pack(side=TOP)
+#button.pack()
+button = Button(frame,text ="A",font = ("Consoles",25),width=3).pack(side=LEFT)
+button = Button(frame,text ="S",font = ("Consoles",25),width=3).pack(side=LEFT)
+button = Button(frame,text ="D",font = ("Consoles",25),width=3).pack(side=LEFT)
+
+window.mainloop()
