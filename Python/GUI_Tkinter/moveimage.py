e3b6e7dbf06c9cc92825dd498e155e91287ef56d
Laiba Memon <memonlaiba03@gmail.com>
2026-08-25 17:12:08 +0200

    Moving

--- Python/GUI_Tkinter/moveimage.py
+++ Python/GUI_Tkinter/moveimage.py
@@ -26,3 +26,14 @@ label= Label(window,image=myimage,bg="red")
 label.place(x=0,y=0)
 
 window.mainloop()
+
+
+#canvas = Canvas(window,width=50,height=50)
+#canvas.pack()
+#photoimage same way
+#myimage = canvas.create_image(0,0,image=Photoimage,anchor=NW)
+#then to move image we use key bindings
+# you use event and functions
+#like to move up and down as above
+#define and use canvas.move(myimage,0,-10) and so on
+
