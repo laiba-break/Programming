c48a742a2318a6a63d297d02a5e844eaf65c2194
Laiba Memon <memonlaiba03@gmail.com>
2026-08-28 11:32:40 +0200

    Pip

---
+++ Python/Advanced/runcommand.py
@@ -0,0 +1,12 @@
+#run.py file with cmd
+#save file as .py
+# go tocommand prompt
+#navigate to directory w-yr file : cd 
+#invoke python intreprator + script : python helloworld.py
+
+
+print("Hello World!")
+
+name = input("Whats your name")
+
+print("Hello"+name)
