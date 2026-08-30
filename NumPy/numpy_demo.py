f7d4a3cd9ce93f88b1b9abfec9b3284cbb91c3e2
Laiba Memon <memonlaiba03@gmail.com>
2026-08-30 11:35:27 +0200

    Numpy

---
+++ NumPy/numpy_demo.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = np.array([1,2,3,4,5],dtype = "i")
+b = np.array((1,6,3,4,9),dtype = "f")
+print(a)
+print(b)
+print(type(a))
+print(type(b))
+print(a.dtype)  #.dtype defines the type of elements in an array 
