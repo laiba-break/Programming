fe2827bea737c35b79c8117002886c7f91054e91
Laiba Memon <memonlaiba03@gmail.com>
2026-08-30 12:37:52 +0200

    Random

---
+++ NumPy/randomdemo.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+A = np.random.permutation(np.arange(10))  #gives 0 to 10 in a random way
+print(A) 
+
+print(np.random.randint(20,30))
+
+v = np.random.randint(20,300)
+print(type(v))
