5178b30e4e183a0d1bb5336a7fe43fa5433da8d0
Laiba Memon <memonlaiba03@gmail.com>
2026-08-30 12:31:04 +0200

    Arange

---
+++ NumPy/nparange.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+a = np.arange(100)
+print(a)
+
+b =np.arange(20,100,3) #start,end,step resembles range
+print(b)
+
+print(range(10)) #in for loop it will iterates and returns the elements
+# so numpy does this and gives it in one line
+#these kind of objects are called iterators
+
+print(list(range(10))) #now it gives u the range of list
+
+
+
