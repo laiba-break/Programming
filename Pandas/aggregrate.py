529b6f50379ded4654c07dae5624fcb8a7de7450
Laiba Memon <memonlaiba03@gmail.com>
2026-09-01 16:08:05 +0200

    Aggregrate Functions

---
+++ Pandas/aggregrate.py
@@ -0,0 +1,28 @@
+#aggregate functions = reduces a set of values into a single summary value
+#used to summarize and analyze data
+#often used with the groupby() function
+import pandas as pd
+df = pd.read_csv("pokemon.csv")
+
+
+#print(df.mean(numeric_only= True)) #find the mean of any numeric coilumn
+#Whole data frame
+#print(df.sum(numeric_only= True)) #find the mean of any numeric coilumn
+#print(df.min(numeric_only= True)) #find the mean of any numeric coilumn
+#print(df.max(numeric_only= True)) #find the max of any numeric coilumn
+#print(df.count())
+
+#Single Column
+#print(df["Height"].mean())
+#print(df["Height"].sum())
+#print(df["Height"].max()) #find the max of any numeric coilumn
+#print(df["Height"].count())
+
+#Group by 
+group = df.groupby("Type1")
+
+#print(group["Height"].mean())
+#print(group["Height"].sum())
+#print(group["Height"].min())
+#print(group["Height"].max())
+print(group["Height"].count())
