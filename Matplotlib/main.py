9c27d395c459c4a1d9dd4f5efddcd15fb4777a2a
Laiba Memon <memonlaiba03@gmail.com>
2026-09-03 12:14:52 +0200

    Basic Plots

---
+++ Matplotlib/main.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#print(matplotlib.__version__)
+
+x = np.array([2023,2024,2025,2026])
+y = np.array([15, 25, 30, 20])
+
+plt.plot(x,y)  #u can add one and they will automitcally give 0.5 increments
+plt.show()
+
+
+
